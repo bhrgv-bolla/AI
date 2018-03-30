0739c2d2cdb8d69409567271b4257629c4c689b7
Bhargav Bolla <hire.bhargav@gmail.com>
2018-03-30 16:37:11 -0700

    incomplete skipgram

--- nlp/tutorials/word2vec.py
+++ nlp/tutorials/word2vec.py
@@ -6,32 +6,37 @@ import numpy as np
 
 
 def skipgram(vocabulary_size, embedding_size):
-    # Initialize embeddings with data that is uniformly distributed.
-    embeddings = tf.Variable(
-        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0)
-        )
+    batch_size = 100 #Run sufficient batch_size until the loss is minimized
 
-    nce_weights = tf.Variable(
-        tf.truncated_normal([vocabulary_size, embedding_size],
-                            stddev=1.0 / math.sqrt(embedding_size))
-        )
+    graph = tf.Graph() #To construct a tensorflow Graph
 
-    nce_biases = tf.Variable(tf.zeros([vocabulaary_size]))
+    with graph.as_default():
+        with tf.name_scope('inputs'):
+            # Placeholders for inputs.
+            train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+            train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
 
-    # Placeholders for inputs.
-    train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-    train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+        with tf.device('/cpu:0'):
+            with tf.name_scode('embeddings'):
+            #Get embedding
+            embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size],
+                                                       -1.0, 1.0))
+            embed = tf.nn.embedding_lookup(embeddings, train_inputs)
 
-    # Snippet to lookup embendings.
-    embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+            with tf.name_scope('weights'):
+                nce_weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size],
+                                                              stddev=1.0/math.sqrt(embedding_size)))
 
-    loss = tf.reduce_mean(
-        tf.nn.nce_loss(
-            weights=nce_weights,
+            with tf.name_scope('biases'):
+                nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+        with tf.name_scope('loss'):
+            loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weights,
              biases=nce_biases,
              labels=train_labels,
              inputs=embed,
              num_sampled=num_sampled,
              num_classes=vocabulary_size))
 
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=1.0).minimize(loss)
+        with tf.name_scope('optimizer'):
+            optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
